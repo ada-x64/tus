414a65d3bdccdf8b463e05e5ada64fef56c3af8f
ada mandala <ada.mandala@pm.me>
2024-07-15 13:32:15 -0500

    log

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-const {Server} = require('@tus/server')
-const {FileStore} = require('@tus/file-store')
+const { Server } = require("@tus/server");
+const { FileStore } = require("@tus/file-store");
 
-const host = '127.0.0.1'
-const port = 1080
+const host = "127.0.0.1";
+const port = 1080;
 const server = new Server({
-  path: '/files',
-  datastore: new FileStore({directory: './files'}),
-})
+  path: "/files",
+  datastore: new FileStore({ directory: "./files" }),
+  onIncomingRequest: async (req, res, id) => {
+    console.log("⬇️ Got request", req.method, "for", id);
+  },
+  onResponseError: async (req, res, err) => {
+    console.log("❌ Error", err);
+    return err;
+  },
+  onUploadCreate: async (req, res, upload) => {
+    console.log("🪣 Upload create\n", upload);
+    return res;
+  },
+  onUploadFinish: async (req, res, upload) => {
+    console.log("✅ Upload finished\n", upload);
+    return res;
+  },
+});
 
-server.listen({host, port})
+server.listen({ host, port });
+console.log(`listening on ${host}:${port}`);
